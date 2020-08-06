2e3db6909afb09adab5c49c414ce8cc6552790ec
Aadarsha <connectwithadarsha@gmail.com>
2020-08-07 02:53:42 +0545

    New : TS with node added

---
+++ 08-Typescript/ts-express/src/app.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from 'express';
+import todoRoutes from './routes/todos';
+
+const app = express();
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hello wolrd');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(3000, () => console.log('server running'));
---
+++ 08-Typescript/ts-express/src/controllers/todos.ts
@@ -0,0 +1,47 @@
+import { RequestHandler } from 'express';
+import { Todo } from '../models/Todo';
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+  TODOS.push(newTodo);
+  res.json({ msg: 'Created new todo', creasstedTodo: newTodo }).status(200);
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) throw new Error("Couldn't find todo.");
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+  res.json({
+    message: 'updated!!',
+    updaredTodo: TODOS[todoIndex],
+  });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) throw new Error("Couldn't find todo.");
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({
+    message: 'Todo deleted',
+  });
+};
